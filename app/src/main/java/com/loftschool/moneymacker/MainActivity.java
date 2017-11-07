ea3a736c542f4315f11c3690a305f44bad9863b6
Mihail Kuzmintsev <kuzmintsev.mihail@gmail.com>
2017-11-07 21:37:01 +0400

    Реализовано все, что сделано на вебинаре(разметка для mainActivity с табами, классы PagerAdapter и ItemsFragment)

--- app/src/main/java/com/loftschool/moneymacker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneymacker/MainActivity.java
@@ -1,11 +1,11 @@
 package com.loftschool.moneymacker;
 
+
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.widget.EditText;
-import android.widget.ImageButton;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,79 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText titleEdit = findViewById(R.id.name);
-        final EditText priceEdit = findViewById(R.id.number);
-        final ImageButton addButton = findViewById(R.id.add);
 
+        ViewPager pager = findViewById(R.id.pages);
+        TabLayout tabs = findViewById(R.id.tabs);
 
-        titleEdit.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (priceEdit.getText().toString().trim().length() != 0) {
-                    addButton.setClickable(true);
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.toString().trim().length() == 0) {
-                    addButton.setClickable(false);
-                }
-            }
-        });
-
-        priceEdit.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (titleEdit.getText().toString().trim().length() != 0) {
-                    addButton.setClickable(true);
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.toString().trim().length() == 0) {
-                    addButton.setClickable(false);
-                }
-            }
-        });
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+        pager.setAdapter(new MainPagerAdapter(getSupportFragmentManager(), getResources()));
+        tabs.setupWithViewPager(pager);
     }
 }
---
+++ app/src/main/java/com/loftschool/moneymacker/MainPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.loftschool.moneymacker;
+
+
+import android.content.res.Resources;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class MainPagerAdapter extends FragmentPagerAdapter {
+
+    private final static int PAGE_EXPENSES = 0;
+    private final static int PAGE_INCOMES = 1;
+    private final static int PAGE_BALANCE = 2;
+
+    private String[] titles;
+
+    public MainPagerAdapter(FragmentManager fm, Resources resourses) {
+        super(fm);
+
+        titles = resourses.getStringArray(R.array.tabs_titles);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case PAGE_EXPENSES: {
+                return ItemsFragment.createItemsFragment(ItemsFragment.TYPE_EXPENSE);
+            }
+            case PAGE_INCOMES: {
+                return ItemsFragment.createItemsFragment(ItemsFragment.TYPE_INCOME);
+            }
+            case PAGE_BALANCE:
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
