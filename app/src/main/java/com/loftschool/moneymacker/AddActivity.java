b7695e98ad1d92d88d6b2941986906b23fd5d962
Mihail Kuzmintsev <kuzmintsev.mihail@gmail.com>
2017-11-13 21:12:02 +0400

    Repaired addButton, fixed FAB

--- app/src/main/java/com/loftschool/moneymacker/AddActivity.java
+++ app/src/main/java/com/loftschool/moneymacker/AddActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import java.util.Objects;
 
 
 public class AddActivity extends AppCompatActivity {
@@ -17,6 +20,9 @@ public class AddActivity extends AppCompatActivity {
     public static final String RESULT_ITEM = "item";
     public static final int RC_ADD_ITEM = 99;
     private String type;
+    private ImageButton addButton;
+    private EditText titleEdit;
+    private EditText priceEdit;
 
 
     @Override
@@ -24,9 +30,9 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        final EditText titleEdit = findViewById(R.id.name);
-        final EditText priceEdit = findViewById(R.id.number);
-        final ImageButton addButton = findViewById(R.id.add);
+        titleEdit = findViewById(R.id.name);
+        priceEdit = findViewById(R.id.number);
+        addButton = findViewById(R.id.add);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -34,64 +40,40 @@ public class AddActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         type = getIntent().getStringExtra(EXTRA_TYPE);
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent result = new Intent();
-                result.putExtra(RESULT_ITEM, new Item(titleEdit.getText().toString(), Integer.valueOf(priceEdit.getText().toString()), type ));
+                String name = titleEdit.getText().toString();
+                int price = (Objects.equals(priceEdit.getText().toString(), "")) ? 0 : Integer.valueOf(priceEdit.getText().toString());
+                result.putExtra(RESULT_ITEM, new Item(name, price, type ));
                 setResult(RESULT_OK, result);
                 finish();
             }
         });
 
-
-
-
-        titleEdit.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (priceEdit.getText().toString().trim().length() != 0) {
-                    addButton.setClickable(true);
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.toString().trim().length() == 0) {
-                    addButton.setClickable(false);
-                }
-            }
-        });
-
-        priceEdit.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (titleEdit.getText().toString().trim().length() != 0) {
-                    addButton.setClickable(true);
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.toString().trim().length() == 0) {
-                    addButton.setClickable(false);
-                }
-            }
-        });
-
+        titleEdit.addTextChangedListener(textWatcher);
+        priceEdit.addTextChangedListener(textWatcher);
+        addButton.setEnabled(false);
     }
+
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            addButton.setEnabled(!TextUtils.isEmpty(titleEdit.getText()) && !TextUtils.isEmpty(priceEdit.getText()));
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
     @Override
     protected void onStart() {
