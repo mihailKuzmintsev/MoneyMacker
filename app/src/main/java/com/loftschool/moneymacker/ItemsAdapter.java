b65b325c98f62f82407a42820b761050c540a6e3
mihailKuzmintsev <kuzma.mishka@gmail.com>
2017-11-18 04:46:37 +0300

    Add ItemsAdapterInterface, create menu for actionMode, realize actionMode for itemLongClick

--- app/src/main/java/com/loftschool/moneymacker/ItemsAdapter.java
+++ app/src/main/java/com/loftschool/moneymacker/ItemsAdapter.java
@@ -1,12 +1,15 @@
 package com.loftschool.moneymacker;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -14,10 +17,16 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHold
 
 
     private List<Item> items = Collections.emptyList();
+    private ItemsAdapterListener listener = null;
+    private SparseBooleanArray selectedItems = new SparseBooleanArray();
 
     public void setItems(List<Item> items) {
         this.items = items;
         notifyDataSetChanged();
+    }
+
+    public void setListener(ItemsAdapterListener listener) {
+        this.listener = listener;
     }
 
     public void updateId(Item item, int id) {
@@ -35,12 +44,44 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHold
     @Override
     public void onBindViewHolder(ItemViewHolder holder, int position) {
         Item item = items.get(position);
-        holder.bind(item);
+        holder.bind(item, position, selectedItems.get(position, false), listener);
     }
 
     @Override
     public int getItemCount() {
         return items.size();
+    }
+
+    void toggleSelection(int pos) {
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+        } else {
+            selectedItems.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    void clearSelections() {
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    int getSelectedItemsCount() {
+        return selectedItems.size();
+    }
+
+    Item remove(int pos) {
+        final Item item = items.remove(pos);
+        notifyItemRemoved(pos);
+        return item;
+    }
+
+    List<Integer> getSelectedItems() {
+        List<Integer> items = new ArrayList<>(getSelectedItemsCount());
+        for (int i = 0; i < getSelectedItemsCount(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
     }
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
@@ -56,9 +97,26 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHold
             price = itemView.findViewById(R.id.item_price);
         }
 
-        void bind(Item item) {
+        void bind(final Item item, final int position, boolean selected, final ItemsAdapterListener listener) {
             price.setText( String.format("%s \u20BD", String.valueOf(item.price)));
             name.setText(item.name);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onItemClick(item, position);
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    listener.onItemLongClick(item, position);
+                    return true;
+                }
+            });
+
+            itemView.setActivated(selected);
         }
     }
 }
---
+++ app/src/main/java/com/loftschool/moneymacker/ItemsAdapterListener.java
@@ -0,0 +1,8 @@
+package com.loftschool.moneymacker;
+
+
+public interface ItemsAdapterListener {
+    void onItemClick(Item item, int position);
+
+    void onItemLongClick(Item item, int position);
+}
